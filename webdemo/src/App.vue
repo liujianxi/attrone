98358b41964447e0de6d541383f0a590d2559cd0
t-one <1968583543@qq.com>
2017-12-05 19:56:37 +0800

    qq-login

--- webdemo/src/App.vue
+++ webdemo/src/App.vue
@@ -3,7 +3,7 @@
 		<keep-alive>
 			<router-view></router-view>
 		</keep-alive>
-		<span @mouseenter="loginMouse()" @mouseleave="loginLeave()" class="login-btn">登录</span>
+		<span id="loginBtn" @mouseenter="loginMouse()" @mouseleave="loginLeave()" class="login-btn">登录</span>
 		<p id="qq_login_btn" @mouseenter="loginMouse()" @mouseleave="loginLeave()" :class="login_flag?'':'not-select'"></p>
 	</div>
 </template>
@@ -14,7 +14,6 @@
 		data(){
 			return{
 				login_flag:false,
-				isLogin:false,//登录之后的判断-true--登陆之后
 			}
 		},
 		created() {
@@ -36,38 +35,18 @@
 		           	nickname : QC.String.escHTML(reqData.nickname), //做xss过滤
 		          	figureurl : reqData.figureurl
 		       	}));
+		       	if(QC.Login.check()){//如果已登录
+					QC.Login.getMe(function(openId, accessToken){
+						console.log(openId,accessToken);
+					});
+					//这里可以调用自己的保存接口
+					//...
+				}
 			},function(){
 				
 			});
 		},
 		methods: {
-			webLoad() {
-				return new Promise(function (resolve, reject) {
-					var paras = {};
-					//用JS SDK调用OpenAPI
-					QC.api("get_user_info", paras)
-					//指定接口访问成功的接收函数，s为成功返回Response对象
-					.success(function(s){
-						//成功回调，通过s.data获取OpenAPI的返回数据
-						alert("获取用户信息成功！当前用户昵称为："+s.data.nickname);
-						console.dir(s.data);
-						resolve(s.data);
-					})
-					//指定接口访问失败的接收函数，f为失败返回Response对象
-					.error(function(f){
-						//失败回调
-						console.log("获取用户信息失败！");
-						reject();
-					})
-					//指定接口完成请求后的接收函数，c为完成请求返回Response对象
-					.complete(function(c){
-						//完成请求回调
-						alert("获取用户信息完成！");
-						window.close();
-						resolve(c);
-					});
-				})
-			},
 			loginMouse(){
 				this.login_flag=true;
 			},
